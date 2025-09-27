c7d555ab131a56a6cecc88637c116844303f1484
maaaaa1245 <124485929+maaaaa1245@users.noreply.github.com>
2025-09-27 14:27:31 +0900

    aaaaaaaaaaa

--- src/main/java/net/maa12345/mekgreenhouse/MekGreenhouse.java
+++ src/main/java/net/maa12345/mekgreenhouse/MekGreenhouse.java
@@ -25,7 +25,7 @@ public class MekGreenhouse
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "mekanism_greenhouse";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public MekGreenhouse(FMLJavaModLoadingContext context)
     {
